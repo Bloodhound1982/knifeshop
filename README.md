e1633c22b1a0d23b3e79d0e8b74db7c343e00bfc
Alex <bloodhound1982@gmail.com>
2016-11-07 06:27:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+KNIFESHOP
+
+Install:
+
+	1. git clone https://github.com/Bloodhound1982/knifeshop
+	2.npm i
+	3.gulp
---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	$('.single-item').slick({
+	//	setting-mame: setting-value
+	dots: true,
+	adaptiveHeight: true
+
+	});
+
+	$('.btn-next').on('click', function() {
+		$('#slick').slick('slickNext');
+	});
+	$('.btn-prev').on('click', function() {
+		$('#slick').slick('slickPrev');
+	});
+
+	var acc = document.getElementsByClassName('accordion');
+
+	for (var i = 0; i < acc.length; i++) {
+		acc[i].onclick = function() {
+			this.classList.toggle("active");
+			this.nextElementSibling.classList.toggle('show');
+		}
+	}
+
+	var image = document.getElementsByClassName("knife_image");
+
+	for (var i = 0; i < image.length; i++) {
+		image[i].addEventListener("click", function() {
+			if (!this.flag) {
+				this.style.width = 250 + "%";
+				this.style.cursor = "zoom-out";
+				this.style.zIndex = "100";
+				this.flag = true;
+			} else {
+				this.style.width = 100 + "%";
+				this.style.cursor = "zoom-in";
+				this.style.zIndex = 1;
+				this.flag = false;
+			}
+		}, false);
+	}
+});
